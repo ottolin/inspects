e6c74f9603ae5ec082f0fe2a0af91c54646bc060
Otto Lin <ottolin@gmail.com>
2015-08-29 11:38:58 +0800

    Adding pmt descriptor parsing

--- lib/parser/psi.ex
+++ lib/parser/psi.ex
@@ -67,12 +67,12 @@ defmodule Parser.Psi do
     <<_prev_section::binary-size(ptr_field), section_bytes::binary>> = rest
     section = Section.parse(section_bytes)
 
-    <<_::3, pcr_pid::13, _::4, pgm_info_len::12, pgm_desc::binary-size(pgm_info_len), stream_info_bytes::binary>> = section.payload
+    <<_::3, pcr_pid::13, _::4, pgm_info_len::12, _pgm_desc::binary-size(pgm_info_len), stream_info_bytes::binary>> = section.payload
     {pcr_pid, get_stream(stream_info_bytes)}
 
   end
 
-  defp get_stream_type(type) do
+  defp get_stream_type(type, desc_list) do
     case type do
       0x01 -> :mpeg2v
       0x02 -> :mpeg2v
@@ -86,7 +86,33 @@ defmodule Parser.Psi do
       0x42 -> :avs
       0xea -> :vc1
       0x86 -> :scte35
-      _    -> :others
+      _    ->
+        Enum.reduce(desc_list, :others,
+          fn ({tag, _len, desc}, acc) ->
+            cond do
+              acc != :others -> acc # Simply return the previous matched type
+              True ->
+                case {type, tag, desc} do
+                  # Registration
+                  {_,    0x05, "CUEI" <> <<_::binary>>} -> :scte35
+                  {_,    0x05, "VANC" <> <<_::binary>>} -> :vanc
+                  {_,    0x05, "BSSD" <> <<_::binary>>} -> :smpte302m
+                  {_,    0x05, "AC-3" <> <<_::binary>>} -> :ac3
+                  # ID3 tag
+                  {_,    0x26, "ID3 " <> <<_::binary>>} -> :id3
+                  # AC3 / E-AC3 in DVB
+                  {_,    0x6a, _} -> :ac3
+                  {_,    0x7a, _} -> :ac3
+                  # Teletext
+                  {_,    0x56, _} -> :teletext
+                  # Subtitle
+                  {_,    0x59, _} -> :subtitle
+                  {0x81, 0x81, _} -> :ac3
+                  # E-AC3 in ATSC
+                  {0x87, 0xcc, _} -> :ac3
+                end
+            end
+          end)
     end
   end
 
@@ -95,13 +121,26 @@ defmodule Parser.Psi do
     get_stream(stream_info_payload, [])
   end
 
-  defp get_stream(<<stream_type_id::8, _::3, pid::13, _::4, es_info_len::12, _descriptor::binary-size(es_info_len), rest::binary>>, streams) do
-    stream_type = get_stream_type(stream_type_id)
+  defp get_stream(<<stream_type_id::8, _::3, pid::13, _::4, es_info_len::12, descriptor::binary-size(es_info_len), rest::binary>>, streams) do
+    desc_list = get_pmt_descriptors(descriptor)
+    stream_type = get_stream_type(stream_type_id, desc_list)
     get_stream(rest, [{pid, stream_type} | streams])
   end
 
-  defp get_stream(dont_care, streams) do
+  defp get_stream(_dont_care, streams) do
     Enum.reverse(streams)
+  end
+
+  defp get_pmt_descriptors(descriptor_bytes) do
+    get_pmt_descriptors(descriptor_bytes, [])
+  end
+
+  defp get_pmt_descriptors(<<desc_tag::8, desc_len::8, desc::binary-size(desc_len), rest::binary>>, descriptor_list) do
+    get_pmt_descriptors(rest, [{desc_tag, desc_len, desc} | descriptor_list])
+  end
+
+  defp get_pmt_descriptors(_done_care, descriptor_list) do
+    Enum.reverse(descriptor_list)
   end
 
   # return [{pid, pgm_num}]
